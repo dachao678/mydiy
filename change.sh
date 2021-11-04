9f167a538d6a91c2e329d59b89b2408eed53409c
dachao678 <74962851+dachao678@users.noreply.github.com>
2021-11-04 21:53:23 +0800

    Update change.sh

--- change.sh
+++ change.sh
@@ -4,7 +4,7 @@ UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 input=y
 while [[ "$input" == "y" ]]
 do
-    result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 5 "https://www.netflix.com/title/70143836" 2>&1)
+    result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/70143836" 2>&1)
     if [[ "$result" == "404" ]];then
         echo -e "IP掉了切换中$(date)"
         systemctl restart wg-quick@wgcf
